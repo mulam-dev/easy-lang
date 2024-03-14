51d0f3cc912caca0e88f6c66e03cdb6af8ce156f
Lane Sun <lanesun@anlbrain.com>
2024-03-14 14:51:31 +0800

    语法: 完成基础集值映射

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-import {init_converter, load_cmrule} from "./utils.js";
-import {parser} from "./parser.js";
+import {init_converter, load_cmrule} from "./chelonia/utils.js";
+import {parser} from "./chelonia/parser.js";
+const __dirname = new URL('.', import.meta.url).pathname;
 
 if (import.meta.main) {
     await init_converter();
-    //
+    const rule = await load_cmrule(__dirname + "rules/easy-lang-base");
+    console.log(parser(rule, `
+        0 1 -2 3.14
+        #T #F
+        \`hello\` \`backseq\n\`
+        \`multiline
+        newline\`
+        token - -> ' $name \\\`
+        @call(1 2)
+        @@rcall(1)(2)
+    `.split('')));
 }
--- rules/easy-lang-base.packer.js
+++ rules/easy-lang-base.packer.js
@@ -1,1 +1,12 @@
-export const packer = {};
+export const packer = {
+    Gap: elems => [],
+    Number: elems => [{id: "Number", value: Number.parseFloat(elems.join(''))}],
+    Boolean: elems => [{id: "Boolean", value: elems[1] === "T"}],
+    String: elems => [{id: "String", value: elems[1]}],
+    StringBody: elems => [elems.join('')],
+    StringBackslashSequence: elems => [elems[1]],
+    Token: elems => [{id: "Token", value: elems.join('')}],
+    TokenBackslashSequence: elems => [elems[1]],
+    Call: elems => [{id: "Call", head: elems[1], body: elems[3]}],
+    CallBody: elems => [elems],
+};
